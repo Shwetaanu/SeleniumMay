e2984ab0a0bac4d2e547669cd668f8558a314e67
gitesh <giteshc@gmail.com>
2025-07-10 12:25:21 +0530

    test

--- src/test/java/TestScripts/baseTestclass.java
+++ src/test/java/TestScripts/baseTestclass.java
@@ -15,24 +15,24 @@ public class baseTestclass {
 	protected WebDriver driver;
 	protected WebDriverWait wait;
 	
-	
-	  @Parameters("browser")
-	   @BeforeMethod public void setup(String browser) 
-	  {
-	  DriverManager.SetDriver(browser); driver=DriverManager.getDriver();
-	  System.out.println(driver);
-	  wait= new WebDriverWait(driver,Duration.ofSeconds(10));
-	  System.out.println("before method called"); 
-	  }
-	 
-	
 	/*
-	 * @BeforeMethod public void setup() { String browser=
-	 * System.getProperty("browser","chrome"); DriverManager.SetDriver(browser);
-	 * driver=DriverManager.getDriver(); System.out.println(driver); wait= new
+	 * @Parameters("browser")
+	 * 
+	 * @BeforeMethod public void setup(String browser) {
+	 * DriverManager.SetDriver(browser); driver=DriverManager.getDriver();
+	 * System.out.println(driver); wait= new
 	 * WebDriverWait(driver,Duration.ofSeconds(10));
 	 * System.out.println("before method called"); }
 	 */
+	
+	
+	  @BeforeMethod public void setup() { 
+		  String browser=	  System.getProperty("browser","chrome");
+		  DriverManager.SetDriver(browser);
+	  driver=DriverManager.getDriver(); System.out.println(driver); wait= new
+	  WebDriverWait(driver,Duration.ofSeconds(10));
+	  System.out.println("before method called"); }
+	 
 	
 	
 	  @AfterMethod() public void tearDown() 
